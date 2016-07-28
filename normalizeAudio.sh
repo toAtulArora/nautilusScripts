27cbf8faf69c97efe127c72e2130dac4fff7294e
Atul Singh Arora <toAtulArora@gmail.com>
2016-07-28 22:41:22 +0530

    forgot to add the actual script!

---
+++ normalizeAudio.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# You'll need zenity, ffmpeg/avconv and ffmpeg-normalize (google it, for me it didn't work with pip)
+if [$# -eq 0]; then
+    zenity --error --title="Oops!" --text="Atlest tell me what to convert!"
+    exit 1
+fi
+norm=$(zenity --entry --title="Width" --text="Input target normalization in dB (-1 is recommended)")
+
+#comList=""
+
+for I in `echo $@`;
+	 #do comList=$comList + "ffmpeg-normalize -u -v -m -l $norm $I &&";
+#do gnome-terminal -x ffmpeg-normalize -u -v -m -l $norm $I;
+do ffmpeg-normalize -u -v -m -l $norm $I;   
+done;
+
+#zenity --info --title=$comList
+
+#comList=$comList + "echo "Done :D"";
+
+#gnome-terminal -x $comList
+	 
